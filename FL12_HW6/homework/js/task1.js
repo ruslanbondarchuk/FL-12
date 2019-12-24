2ee07b1220d56007d1d7552f14fa414cd55df05b
Ruslan Bondarchuk <ru88seg@gmail.com>
2019-12-24 22:34:46 +0200

    magic numbers correction

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,11 +1,13 @@
 alert('solving a quadratic equation "ax2 + bx + c = 0" \n Please, input  numbers a, b and c');
+const two = 2;
+const four =4;
 let prompta = prompt('please, input a');
 let a = parseFloat(prompta);
 let promptb = prompt('Please, input b');
 let b = parseFloat(promptb);
 let promptc = prompt('Please, input c');
 let c = parseFloat(promptc);
-let dis = Math.pow(b, 2) - 4 * a * c ;
+let dis = Math.pow(b, two) - four * a * c ;
 if (dis < 0) {
 console.log('equation has no solution, \n Discriminant='+ dis);
 alert('No soution, please check results in console');
@@ -20,8 +22,8 @@ alert('No soution, please check results in console');
   alert('error, please check results in console');
 } else {
     console.log('a = ' + a + ' b = ' + b + ' c= ' + c + ' Discriminant = ' + dis );
-    let x1 = (-b + Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a);
-    let x2 = (-b - Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a);
+    let x1 = (-b + Math.sqrt(Math.pow(b,two)-four*a*c))/(two*a);
+    let x2 = (-b - Math.sqrt(Math.pow(b,two)-four*a*c))/(four*a);
     console.log('x1 = ' + Math.round(x1) + ', x2 =' + Math.round(x2));
     alert('please check results in console');
 }
